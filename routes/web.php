bda2dee48f7f13c4a32f2f2dd1e72b8b61a836d0
maxdub <dubinovsky.max@gmail.com>
2022-03-18 13:20:29 -0400

    remove a tags on posts

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('posts/{post}', function ($slug) {
         return abort(404);
     }
 
-    $post = cache()->remember("posts.${slug}", now()->addDay(), fn () => file_get_contents($path);
+    $post = cache()->remember("posts.${slug}", now()->addDay(), fn () => file_get_contents($path));
 
     return view('post', [
         'post' => $post
