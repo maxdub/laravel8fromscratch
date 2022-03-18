e8bc91367f7512040490ee0ef05a3242cdeaa1f2
maxdub <dubinovsky.max@gmail.com>
2022-03-17 20:59:45 -0400

    add wild card posts to route and static html resources

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur corporis debitis dolore doloribus ea fugit labore laudantium nobis placeat qui! Et hic, inventore nobis nostrum quidem repellat reprehenderit similique vel!
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, pariatur, suscipit! Adipisci aliquam, animi atque corporis expedita explicabo, fugiat ipsum nobis non odio quaerat quam quo repellendus reprehenderit, voluptate voluptatum.
@@ -15,7 +15,7 @@
         </p>
     </article>
     <article>
-        <h1><a href="/post">My Second Post</a></h1>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur corporis debitis dolore doloribus ea fugit labore laudantium nobis placeat qui! Et hic, inventore nobis nostrum quidem repellat reprehenderit similique vel!
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, pariatur, suscipit! Adipisci aliquam, animi atque corporis expedita explicabo, fugiat ipsum nobis non odio quaerat quam quo repellendus reprehenderit, voluptate voluptatum.
@@ -23,7 +23,7 @@
         </p>
     </article>
     <article>
-        <h1><a href="/post">My Third Post</a></h1>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur corporis debitis dolore doloribus ea fugit labore laudantium nobis placeat qui! Et hic, inventore nobis nostrum quidem repellat reprehenderit similique vel!
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, pariatur, suscipit! Adipisci aliquam, animi atque corporis expedita explicabo, fugiat ipsum nobis non odio quaerat quam quo repellendus reprehenderit, voluptate voluptatum.
