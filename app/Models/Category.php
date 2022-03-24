ac692cfdc3058d45ace39c5047ced5964bf2287a
maxdub <dubinovsky.max@gmail.com>
2022-03-24 16:31:44 -0400

    add slug to categories

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public function posts()
+    {
+        //hasOne, hasMany, belongsTo, belongsToMany
+        $posts = $this->hasMany(Post::class);
+        return $posts;
+    }
+
+    public function getRouteKeyName() : string
+    {
+        return 'slug';
+    }
+}
