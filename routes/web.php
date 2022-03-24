366d687ca182b55d313d581ed3eee3e96851913b
maxdub <dubinovsky.max@gmail.com>
2022-03-24 15:54:04 -0400

    add slug to categories

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -15,14 +16,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $posts =  Post::all();
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => $posts
     ]);
 });
 
 Route::get('posts/{post}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
 //-> whereAlpha('post');
